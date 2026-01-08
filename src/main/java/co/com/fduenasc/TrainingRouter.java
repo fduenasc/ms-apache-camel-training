df76b9a0950f0ef6e0a20705eab689e43f91f5bc
Francisco Dueñas <Sebasduca@gmail.com>
2026-01-08 12:22:40 -0500

    feat: Implement TrainingRouter to configure a basic Apache Camel route for logging messages

---
+++ src/main/java/co/com/fduenasc/TrainingRouter.java
@@ -0,0 +1,27 @@
+package co.com.fduenasc;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.builder.RouteBuilder;
+import org.jboss.logging.Logger;
+
+/**
+ * TrainingRouter is a class that configures a basic Apache Camel route
+ * to print a message to the console.
+ */
+@ApplicationScoped
+public class TrainingRouter extends RouteBuilder {
+
+    private static final Logger LOGGER = Logger.getLogger(TrainingRouter.class);
+
+    @Override
+    public void configure() throws Exception {
+        // Basic route that prints "The North Remembers!" to the console
+        from("timer:training?repeatCount=100&delay=1000")
+                .setBody(constant("The North Remembers!"))
+                .process(exchange -> {
+                    String message = exchange.getIn().getBody(String.class);
+                    LOGGER.info(message);
+                });
+    }
+}
+
