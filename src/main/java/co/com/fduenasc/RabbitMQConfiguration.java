825690e6486e8fb936a992e783e882d532f49093
Francisco Dueñas <Sebasduca@gmail.com>
2026-01-19 16:54:31 -0500

    refactor: Enhance RabbitMQConfiguration with default values and null safety for connection properties

--- src/main/java/co/com/fduenasc/RabbitMQConfiguration.java
+++ src/main/java/co/com/fduenasc/RabbitMQConfiguration.java
@@ -7,6 +7,8 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
+import java.util.Objects;
+
 /**
  * Configuration class for RabbitMQ ConnectionFactory
  * This provides the Spring AMQP ConnectionFactory required by the spring-rabbitmq component
@@ -14,26 +16,31 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 @ApplicationScoped
 public class RabbitMQConfiguration {
 
-    @ConfigProperty(name = "rabbitmq.host", defaultValue = "localhost")
+    private static final String DEFAULT_HOST = "localhost";
+    private static final String DEFAULT_USERNAME = "guest";
+    private static final String DEFAULT_PASSWORD = "guest";
+
+    @ConfigProperty(name = "rabbitmq.host", defaultValue = DEFAULT_HOST)
     String host;
 
     @ConfigProperty(name = "rabbitmq.port", defaultValue = "5672")
     int port;
 
-    @ConfigProperty(name = "rabbitmq.username", defaultValue = "guest")
+    @ConfigProperty(name = "rabbitmq.username", defaultValue = DEFAULT_USERNAME)
     String username;
 
-    @ConfigProperty(name = "rabbitmq.password", defaultValue = "guest")
+    @ConfigProperty(name = "rabbitmq.password", defaultValue = DEFAULT_PASSWORD)
     String password;
 
     @Produces
     @ApplicationScoped
+    @SuppressWarnings("null") // Objects.requireNonNullElse() guarantees non-null return when defaultValue is non-null
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory factory = new CachingConnectionFactory();
-        factory.setHost(host);
+        factory.setHost(Objects.requireNonNullElse(host, DEFAULT_HOST));
         factory.setPort(port);
-        factory.setUsername(username);
-        factory.setPassword(password);
+        factory.setUsername(Objects.requireNonNullElse(username, DEFAULT_USERNAME));
+        factory.setPassword(Objects.requireNonNullElse(password, DEFAULT_PASSWORD));
         return factory;
     }
 }
